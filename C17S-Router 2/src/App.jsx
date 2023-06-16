8071eb6650dff706a9606464b008051a5e70db91
lobmar146 <lobmar146@gmail.com>
2023-06-16 12:07:47 -0300

    Fixex c17 con rutas anidadas baby

--- C17S-Router 2/src/App.jsx
+++ C17S-Router 2/src/App.jsx
@@ -1,19 +1,29 @@
 import { useState } from 'react'
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link, Outlet } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
 import Ejercicio1 from './routes/Ejercicio1'
 import Ejercicio2 from './routes/Ejercicio2'
+import PostEspecifico from './components/PostEspecifico'
+import ListarPosts from './components/ListarPosts'
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
+        {/* Pagina index, igual que ejercicio 1 */}
         <Route path='/' element={<Ejercicio1 />}>
-          <Route path='ejercicio1' element={<Ejercicio1 />} />
-          <Route path='ejercicio2' element={<Ejercicio2 />} />
+          <Route index element={<ListarPosts />} />
+          <Route path=':id' element={<PostEspecifico />} />
         </Route>
+        {/* Pagina Ejercicio 1 */}
+        <Route path='/ejercicio1' element={<Ejercicio1 />}>
+          <Route index element={<ListarPosts />} />
+          <Route path=':id' element={<PostEspecifico />} />
+        </Route>
+        {/* Ejercicio 2 */}
+        <Route path='/ejercicio2' element={<Ejercicio2 />} />
       </Routes>
     </>
   )
