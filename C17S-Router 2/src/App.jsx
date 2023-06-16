b47f66bb80576dd39fcd81c8c21d5898159989ab
lobmar146 <lobmar146@gmail.com>
2023-06-16 17:34:18 -0300

    Subiendo MT17 eje2

--- C17S-Router 2/src/App.jsx
+++ C17S-Router 2/src/App.jsx
@@ -2,10 +2,11 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
 import Posteos from './routes/Posteos'
-import Ejercicio2 from './routes/Ejercicio2'
+import Juegos from './routes/Juegos'
 import PostEspecifico from './components/ejercicio1/PostEspecifico'
 import ListarPosts from './components/ejercicio1/ListarPosts'
 import FormularioGames from './components/ejercicio2/FormularioGames'
+import JuegoEspecifico from './components/ejercicio2/JuegoEspecifico'
 
 function App() {
   return (
@@ -21,12 +22,10 @@ function App() {
           <Route path=':id' element={<PostEspecifico />} />
         </Route>
         {/* Ejercicio 2 */}
-        <Route path='/ejercicio2' element={<Ejercicio2 />}>
-          <Route index element={<FormularioGames />} />
-        </Route>
+        <Route path='/juegos' element={<Juegos />} />
+        <Route path='/juegos/:id' element={<JuegoEspecifico />} />
       </Routes>
     </>
   )
 }
-
 export default App
